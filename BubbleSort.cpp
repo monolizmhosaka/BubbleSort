b95c13ea170b6f51ffd765333e46f1f8c5ce4fbd
monolizm-pc1 <hosaka@monolizm.com>
2023-10-10 15:06:25 +0900

    動的確保のコードを追加

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,37 +1,62 @@
 ﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+#define USER_INPUT
+
+using namespace std;
 
 void bubbleSort(int array[], int array_size) {
-    int i, j, k;
-
-    for (i = 0; i < (array_size - 1); i++) {
-        for (j = (array_size - 1); j > i; j--) {
+    for (int i = 0; i < (array_size - 1); i++) {
+        for (int j = (array_size - 1); j > i; j--) {
             if (array[j] < array[j - 1]) {
-                k = array[j];
-                array[j] = array[j - 1];
-                array[j - 1] = k;
+                std::swap(array[j], array[j - 1]);
             }
         }
     }
 }
 
+void showArray(int* pArr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << pArr[i] << ",";
+    }
+    cout << endl;
+}
+
 int main(void) {
 
-    int i;
+    int size = 0;
+    int* pArray = NULL;
+
+    srand((unsigned int)time(NULL));
+
+    cout << "バブルソートを行います。" << endl;
+#ifdef USER_INPUT
+    cout << "値の個数を入力してください。" << endl;
+    cin >> size;
+
+    // 配列を確保する
+    pArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        pArray[i] = rand() % 99 + 1;
+    }
+
+#else
     int array[10] = { 3, 6, 1, 7, 2, 0, 4, 5, 9, 8 };
+    pArray = array;
+    size = sizeof(array) / sizeof(array[0]);
+#endif
 
-    printf("       array: ");
-    for (i = 0; i < sizeof(array) / sizeof(array[0]); i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
 
-    bubbleSort(array, sizeof(array) / sizeof(array[0]));
+    showArray(pArray, size);
 
-    printf("sorted array: ");
-    for (i = 0; i < sizeof(array) / sizeof(array[0]); i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
+    bubbleSort(pArray, size);
+    cout << "↓" << endl;
+
+    showArray(pArray, size);
 
     return 0;
 }
